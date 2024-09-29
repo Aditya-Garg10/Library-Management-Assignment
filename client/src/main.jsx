ca52444cf1048b8c9261b8f61b2b7162c19eb222
Aditya-Garg10 <adityagarg646@gmail.com>
2024-09-29 14:21:42 +0530

    msm

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import DataContextProvider from './context/context.jsx'
+import DataContextProvider from './context/Context.jsx'
 
 
 createRoot(document.getElementById('root')).render(
