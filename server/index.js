011fd48ceee2ef7bb070c6244b54ddbb9cc50e72
Aditya-Garg10 <adityagarg646@gmail.com>
2024-09-29 14:47:30 +0530

    msms

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ const port = process.env.port
 
 app.use(cors({
     origin: [                       
-      "https://library-management-assignment-al0npq4bt.vercel.app"              
+      "https://library-management-assignment-al0npq4bt.vercel.app" ,
+      "https://library-management-assignment-z9tq.vercel.app"           
       ],    
     methods:["GET","POST","DELETE","PUT","PATCH"],
     credentials: true,
@@ -26,7 +27,8 @@ app.use(cors({
 
 
 const allowedOrigins = [             
-  "https://library-management-assignment-al0npq4bt.vercel.app"
+  "https://library-management-assignment-al0npq4bt.vercel.app",
+  "https://library-management-assignment-z9tq.vercel.app"
   ];
   
 app.use((req, res, next) => {
